f4b016c77e40b5ab8735cbe8f71bc20bc830f723
roshk99 <roshni.s.kaushik@gmail.com>
2015-02-01 10:29:24 -0600

    messing up networking

--- MissingSocks/android/app/src/main/java/com/aelsey/missingsocks/add_edit_sock.java
+++ MissingSocks/android/app/src/main/java/com/aelsey/missingsocks/add_edit_sock.java
@@ -4,6 +4,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,12 +20,27 @@ import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 
 public class add_edit_sock extends ActionBarActivity {
@@ -38,11 +55,11 @@ public class add_edit_sock extends ActionBarActivity {
         setTitle("Add/Edit Sock");
     }
 
-    public void backToBrowse(View view)
-    {
+    public void backToBrowse(View view) {
         Intent intent = new Intent(this, Browse.class);
         startActivity(intent);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -65,7 +82,47 @@ public class add_edit_sock extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void testClick(View v) {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadFromNetwork(urls[0]);
+            } catch (IOException e) {
+                return "ConnectionError";
+            }
+        }
+
+        /**
+         * Uses the logging framework to display the output of the fetch
+         * operation in the log fragment.
+         */
+        @Override
+        //protected void onPostExecute(String result) {
+        //Log.i(TAG, result);
+        //}
+    }
+
+    /**
+     * Initiates the fetch operation.
+     */
+    public String loadFromNetwork(String urlString) throws IOException {
+        InputStream stream = null;
+        String str = "";
+
+        try {
+            stream = downloadUrl(urlString);
+            str = readIt(stream, 500);
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+        return str;
+    }
+
+    public void testClick(View v) throws IOException {
         // The code to be executed on click
 
         EditText date_obj = (EditText) findViewById(R.id.date);
@@ -86,10 +143,10 @@ public class add_edit_sock extends ActionBarActivity {
 
         RadioGroup radio1_obj = (RadioGroup) findViewById(R.id.radiogroup);
         int selectedId = radio1_obj.getCheckedRadioButtonId();
-        View radbutton=  findViewById(selectedId);
+        View radbutton = findViewById(selectedId);
         String radio1_longstring = radbutton.toString();
         int idx = radio1_longstring.indexOf("id/");
-        String radio1_selected = radio1_longstring.substring(idx+3, radio1_longstring.length() - 1);
+        String radio1_selected = radio1_longstring.substring(idx + 3, radio1_longstring.length() - 1);
         System.out.println(radio1_selected);
 
         Spinner style_obj = (Spinner) findViewById(R.id.style_dropdown);
@@ -103,60 +160,50 @@ public class add_edit_sock extends ActionBarActivity {
         EditText description_obj = (EditText) findViewById(R.id.descriptiontext);
         CharSequence description_val = description_obj.getText();
         System.out.println(description_val);
-
-
-        JSONObject json = new JSONObject();
+    }
+        /*
+        URL url;
+        HttpURLConnection conn;
+        BufferedReader rd;
+        String line;
+        String urlToRead = "http://www.google.com";
+        String result = "";
         try {
-            json.put("someKey", "someValue");
-        }
-        catch(JSONException e) {
-            System.out.println("JSON error");
-        }
-
-        HttpClient httpClient = new DefaultHttpClient();
-
-        try {
-            HttpPost request = new HttpPost("http://yoururl");
-            StringEntity params = new StringEntity(json.toString());
-            request.addHeader("content-type", "application/json");
-            request.setEntity(params);
-            httpClient.execute(request);
-        // handle response here...
-        } catch (Exception ex) {
-            // handle exception here
-        } finally {
-            httpClient.getConnectionManager().shutdown();
-        }
-
-        /*if (date_val == null){
-            alertmessage("Blank Date");
+            url = new URL(urlToRead);
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+                result += line;
+            }
+            rd.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }*/
 
+    /**
+     * Given a string representation of a URL, sets up a connection and gets
+     * an input stream.
+     *
+     * @param urlString A string representation of a URL.
+     * @return An InputStream retrieved from a successful HttpURLConnection.
+     * @throws java.io.IOException
+     */
+    public InputStream downloadUrl(String urlString) throws IOException {
+        // BEGIN_INCLUDE(get_inputstream)
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Start the query
+        conn.connect();
+        InputStream stream = conn.getInputStream();
+        return stream;
+        // END_INCLUDE(get_inputstream)
     }
-    /*public void alertmessage(String message) {
 
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                context);
-
-        // set title
-        alertDialogBuilder.setTitle("Invalid Input");
-
-        // set dialog message
-        alertDialogBuilder
-                .setMessage(message)
-                .setCancelable(false)
-                .setNegativeButton("Ok",new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        // if this button is clicked, just close
-                        // the dialog box and do nothing
-                        dialog.cancel();
-                    }
-                });
-
-        // create alert dialog
-        AlertDialog alertDialog = alertDialogBuilder.create();
-
-        // show it
-        alertDialog.show();
-    }*/
 }
