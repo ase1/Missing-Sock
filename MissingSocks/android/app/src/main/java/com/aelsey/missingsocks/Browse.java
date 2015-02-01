1a60b2cf725e889070b2b4be54fb530ab8fb375d
Andrew Elsey <ase1@rice.edu>
2015-01-31 19:40:18 -0600

    progress on listview

--- MissingSocks/android/app/src/main/java/com/aelsey/missingsocks/Browse.java
+++ MissingSocks/android/app/src/main/java/com/aelsey/missingsocks/Browse.java
@@ -1,19 +1,74 @@
 package com.aelsey.missingsocks;
 
+import android.support.v7.app.ActionBarActivity;
+
+package com.example.androidhive;
+
 import android.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import android.app.Activity;
-
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+import com.aelsey.missingsocks.LazyAdapter;
 
 public class Browse extends ActionBarActivity {
-//    ListView listView;
+    // All static variables
+    static final String URL = "http://api.androidhive.info/music/music.xml";
+    // XML node keys
+    static final String KEY_SONG = "song"; // parent node
+    static final String KEY_ID = "id";
+    static final String KEY_TITLE = "title";
+    static final String KEY_ARTIST = "artist";
+    static final String KEY_DURATION = "duration";
+    static final String KEY_THUMB_URL = "thumb_url";
+
+    ListView listView;
+    LazyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        ArrayList<HashMap> songsList = new ArrayList<HashMap>();
+
+        XMLParser parser = new XMLParser();
+        String xml = parser.getXmlFromUrl(URL); // getting XML from URL
+        Document doc = parser.getDomElement(xml); // getting DOM element
+
+        NodeList nl = doc.getElementsByTagName(KEY_SONG);
+        // looping through all song nodes &lt;song&gt;
+        for (int I = 0; I &lt; nl.getLength(); i++) {
+            // creating new HashMap
+            HashMap&lt;String, String&gt; map = new HashMap&lt;String, String&gt;();
+            Element e = (Element) nl.item(i);
+            // adding each child node to HashMap key =&gt; value
+            map.put(KEY_ID, parser.getValue(e, KEY_ID));
+            map.put(KEY_TITLE, parser.getValue(e, KEY_TITLE));
+            map.put(KEY_ARTIST, parser.getValue(e, KEY_ARTIST));
+            map.put(KEY_DURATION, parser.getValue(e, KEY_DURATION));
+            map.put(KEY_THUMB_URL, parser.getValue(e, KEY_THUMB_URL));
+
+            // adding HashList to ArrayList
+            songsList.add(map);
+        }
+        ;
+        listView = (ListView) findViewById(R.id.listView);
+        String[] values = new String[] { "White Crew Sock\nJones Laundry Room\nSubmitted Yesterday", "list item 2"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, values);
+        listView.setAdapter(adapter);
     }
 
 
